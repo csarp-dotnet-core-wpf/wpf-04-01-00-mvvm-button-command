5c35f37be3b52e0faacce857cec3ccf3b5104317
Csaba Gyuris <gyuris.csaba@vasvari.org>
2023-09-14 10:23:50 +0200

    A konstrktor egy egységnyi oldalú négyzetet példányosít.

--- RectangleProject/ViewModels/RectangleViewModel.cs
+++ RectangleProject/ViewModels/RectangleViewModel.cs
@@ -5,5 +5,10 @@ namespace RectangleProject.ViewModels
     public class RectangleViewModel
     {
         private Rectangle _rectangle;
+
+        public RectangleViewModel()
+        {
+            _rectangle = new Rectangle(1, 1);
+        }
     }
 }
