5f2fdb8181a2f378b13d12a02a14d0e60ea38eee
Csaba Gyuris <gyuris.csaba@vasvari.org>
2023-09-14 09:49:22 +0200

    A Rectangle osztály konstruktora

--- RectangleProject/Models/Rectangle.cs
+++ RectangleProject/Models/Rectangle.cs
@@ -4,5 +4,11 @@
     {
         private double _sideA;
         private double _sideB;
+
+        public Rectangle(double sideA, double sideB)
+        {
+            _sideA = sideA;
+            _sideB = sideB;
+        }
     }
 }
