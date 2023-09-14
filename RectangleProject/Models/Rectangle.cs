15a78509b77b05d6a3b73c0f300ee1d4dc6150ac
Csaba Gyuris <gyuris.csaba@vasvari.org>
2023-09-14 09:52:45 +0200

    A két számított property

--- RectangleProject/Models/Rectangle.cs
+++ RectangleProject/Models/Rectangle.cs
@@ -13,5 +13,15 @@
 
         public double SideA { get => _sideA; set => _sideA = value; }
         public double SideB { get => _sideB; set => _sideB = value; }
+
+        public double Area
+        {
+            get { return _sideA * _sideB; }
+        }
+
+        public double Perimeter
+        {
+            get { return 2 * (_sideA + _sideB); }
+        }
     }
 }
